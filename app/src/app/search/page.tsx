bb878fd65b04caf7bb1b68be40bb956b835c017b
Robert Gaal <robertgaal@gmail.com>
2025-10-08 15:57:00 +0200

    Add a loading message in Dutch to inform users that the search may take up to a minute.

--- app/src/app/search/page.tsx
+++ app/src/app/search/page.tsx
@@ -149,6 +149,7 @@ function SearchContent() {
           <div className="text-center py-12">
             <div className="inline-block animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
             <p className="mt-4 text-gray-600">{loadingMessage}</p>
+            <p className="mt-2 text-sm text-gray-500">Dit kan tot een minuut duren</p>
           </div>
         )}
 
